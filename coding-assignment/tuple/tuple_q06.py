47346defc06706b57c0fc5b19c04582b0fd45709
Darshitbansal02 <darshitbansal2004@gmail.com>
2024-11-28 19:23:46 +0530

    Unpack a Tuple into Variables

---
+++ coding-assignment/tuple/tuple_q06.py
@@ -0,0 +1,5 @@
+# 6. Unpack a Tuple into Variables
+# Write a program to unpack a tuple into individual variables.
+input_tuple = (10, 20, 30)
+a, b, c = input_tuple
+print(a, b, c)  # Output: 10 20 30
