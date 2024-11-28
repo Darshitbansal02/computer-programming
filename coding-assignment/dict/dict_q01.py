a37145066e8e9998ac97f1a8c378a977a0607147
Darshitbansal02 <darshitbansal2004@gmail.com>
2024-11-28 16:46:46 +0530

    Update dict_q01.py

--- coding-assignment/dict/dict_q01.py
+++ coding-assignment/dict/dict_q01.py
@@ -1,6 +1,6 @@
-1. Nested Dictionary: Accessing Values in Nested Dictionaries
+# 1. Nested Dictionary: Accessing Values in Nested Dictionaries
 
-Problem: You have a dictionary with nested dictionaries. You need to access values from the nested dictionaries.
+# Problem: You have a dictionary with nested dictionaries. You need to access values from the nested dictionaries.
 
 my_dict = {
     'person1': {'name': 'John', 'age': 25},
