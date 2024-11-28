ba94ce7c71f5457b9cfa839f6e01ce30ff13142d
Darshitbansal02 <darshitbansal2004@gmail.com>
2024-11-28 18:45:50 +0530

    Swap the Case of Each Character

---
+++ coding-assignment/string/string_q02.py
@@ -0,0 +1,5 @@
+# 2. Swap the Case of Each Character
+# Write a program to swap the case of each character in the given string.
+string = "Hello World"
+swapped_string = string.swapcase()
+print(swapped_string)  # Output: "hELLO wORLD"
