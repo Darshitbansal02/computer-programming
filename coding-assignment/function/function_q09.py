069168af4f6bf5231fbd2acfb3650d4db411f3a8
Darshitbansal02 <darshitbansal2004@gmail.com>
2024-11-28 17:46:12 +0530

    Function to compute the area of a circle

---
+++ coding-assignment/function/function_q09.py
@@ -0,0 +1,9 @@
+# 9. Write a function to calculate the area of a circle.
+
+import math
+
+def circle_area(radius):
+    return math.pi * radius ** 2
+
+# Example usage
+print(circle_area(5))  # Output: 78.53981633974483
