38dd56f9663405365987408508cfa08734d40e9b
Darshitbansal02 <darshitbansal2004@gmail.com>
2024-11-28 18:56:30 +0530

    Palindrome

---
+++ coding-assignment/string/string_q17.py
@@ -0,0 +1,14 @@
+# 17. Check for a Valid Palindrome
+# Write a program to check if a string is a valid palindrome, ignoring spaces, punctuation, and case.
+import re
+string = "A man, a plan, a canal, Panama"
+clean_string = re.sub(r'[^A-Za-z]', '', string).lower()
+is_palindrome = True
+for i in range(len(clean_string) // 2):
+    if clean_string[i] != clean_string[-i - 1]:
+        is_palindrome = False
+        break
+if is_palindrome:
+    print("Valid Palindrome")  # Output: "Valid Palindrome"
+else:
+    print("Not a Valid Palindrome")
