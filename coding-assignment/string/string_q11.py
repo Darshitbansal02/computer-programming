f198144efa42a6e9a1f94a542eb49679cc98f435
Darshitbansal02 <darshitbansal2004@gmail.com>
2024-11-28 19:07:55 +0530

    Number of Words in a String

--- coding-assignment/string/string_q11.py
+++ coding-assignment/string/string_q11.py
@@ -1,7 +1,7 @@
 # 11. Count the Number of Words in a String
 # Write a program to count the number of words in a given string.
-string = "Hello world, how are you?"
+input_string = "Hello world, how are you?"
 word_count = 0
-for word in string.split():
+for word in input_string.split():
     word_count += 1
 print("Word count:", word_count)  # Output: 5
