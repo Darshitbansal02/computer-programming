1f5eb00536a7c5298eae2b84928793d202b3f9a1
Darshitbansal02 <darshitbansal2004@gmail.com>
2024-11-28 18:35:44 +0530

    Elements that Appear More Than Once in a List

--- coding-assignment/list/list_q12.py
+++ coding-assignment/list/list_q12.py
@@ -1,4 +1,4 @@
-# 12. Check if a List is Sorted in Ascending Order
-nums = [1, 2, 3, 4, 5]
-is_sorted = all(nums[i] <= nums[i+1] for i in range(len(nums)-1))
-print(is_sorted)  # Output: True
+# 12. Find the Elements that Appear More Than Once in a List
+nums = [1, 2, 3, 4, 5, 6, 3, 7, 8, 1]
+duplicates = [num for num in set(nums) if nums.count(num) > 1]
+print(duplicates)  # Output: [1, 3]
