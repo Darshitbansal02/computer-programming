e26bcc725dcf90b3d74e21f4e2bd8c3fe6b0d9fd
Darshitbansal02 <darshitbansal2004@gmail.com>
2024-11-28 19:16:54 +0530

    Merge Two Tuples

---
+++ coding-assignment/tuple/tuple_q02.py
@@ -0,0 +1,6 @@
+# 2. Merge Two Tuples
+# Write a program to merge two tuples into one.
+tuple1 = (1, 2, 3)
+tuple2 = (4, 5, 6)
+merged_tuple = tuple1 + tuple2
+print(merged_tuple)  # Output: (1, 2, 3, 4, 5, 6)
