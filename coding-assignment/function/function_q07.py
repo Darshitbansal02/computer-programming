71692511f4470c4d77bab1933ec7a0500d07a573
Darshitbansal02 <darshitbansal2004@gmail.com>
2024-11-28 17:19:51 +0530

    Function that checks if a string is the same forward and backward (palindrome)

---
+++ coding-assignment/function/function_q07.py
@@ -0,0 +1,8 @@
+# 7. Define a function that checks if a string is a palindrome.
+
+def is_palindrome(s):
+    return s == s[::-1]
+
+# Example usage
+print(is_palindrome("racecar"))  # Output: True
+print(is_palindrome("hello"))  # Output: False
