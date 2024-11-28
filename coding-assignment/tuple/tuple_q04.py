7571174051d78f7310db3354d8dd7b14455a84f8
Darshitbansal02 <darshitbansal2004@gmail.com>
2024-11-28 19:17:58 +0530

    Slice a Tuple

---
+++ coding-assignment/tuple/tuple_q04.py
@@ -0,0 +1,5 @@
+# 4. Slice a Tuple
+# Write a program to slice a tuple and extract a part of it.
+input_tuple = (1, 2, 3, 4, 5, 6)
+sliced_tuple = input_tuple[2:5]
+print(sliced_tuple)  # Output: (3, 4, 5)
