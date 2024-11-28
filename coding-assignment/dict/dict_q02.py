161c6eb4d2335a7b84fa786387ae9add2a7daeff
Darshitbansal02 <darshitbansal2004@gmail.com>
2024-11-28 16:47:14 +0530

    Update dict_q02.py

--- coding-assignment/dict/dict_q02.py
+++ coding-assignment/dict/dict_q02.py
@@ -1,6 +1,6 @@
-2. Default Dictionary Values using get()
+# 2. Default Dictionary Values using get()
 
-Problem: You need to safely access values from a dictionary, but the key may not exist. Use get() to avoid KeyError if the key is missing.
+# Problem: You need to safely access values from a dictionary, but the key may not exist. Use get() to avoid KeyError if the key is missing.
 
 my_dict = {'name': 'Bob', 'age': 22}
 print(my_dict.get('age'))         # Output: 22
