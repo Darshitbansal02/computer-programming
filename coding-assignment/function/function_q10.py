bb0944b370537eb5b832b8013d1d0950e944ca7d
Darshitbansal02 <darshitbansal2004@gmail.com>
2024-11-28 17:48:59 +0530

    Function that calculates the average

---
+++ coding-assignment/function/function_q10.py
@@ -0,0 +1,7 @@
+# 10. Write a function that takes a list of integers and returns their average.
+
+def average(numbers):
+    return sum(numbers) / len(numbers)
+
+# Example usage
+print(average([1, 2, 3, 4, 5]))  # Output: 3.0
