af27e94a1d45e9c7b327ecb7837b46d411497a34
Darshitbansal02 <darshitbansal2004@gmail.com>
2024-11-28 19:03:58 +0530

    Reverse Each Word in a String

--- coding-assignment/string/string_q04.py
+++ coding-assignment/string/string_q04.py
@@ -1,5 +1,8 @@
 # 4. Reverse Each Word in a String
 # Write a program to reverse each word in a sentence while keeping the word order intact.
-string = "hello world"
-reversed_words = ' '.join(word[::-1] for word in string.split())
-print(reversed_words)  # Output: "olleh dlrow"
+input_string = "hello world"
+reversed_words = ""
+words = input_string.split()
+for word in words:
+    reversed_words += word[::-1] + " "
+print(reversed_words.strip())  # Output: "olleh dlrow"
