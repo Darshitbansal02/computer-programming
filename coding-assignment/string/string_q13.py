c62d5a2f67abb5f51fe0215272cef3b6df6e9524
Darshitbansal02 <darshitbansal2004@gmail.com>
2024-11-28 18:53:52 +0530

    Find the Longest Word in a String

---
+++ coding-assignment/string/string_q13.py
@@ -0,0 +1,9 @@
+# 13. Find the Longest Word in a String
+# Write a program to find the longest word in a sentence.
+string = "The quick brown fox jumps over the lazy dog"
+words = string.split()
+longest_word = ""
+for word in words:
+    if len(word) > len(longest_word):
+        longest_word = word
+print("Longest word:", longest_word)  # Output: "jumps"
