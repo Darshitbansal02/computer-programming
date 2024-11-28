c80b91f46871a92ebab22930b251343016045561
Darshitbansal02 <darshitbansal2004@gmail.com>
2024-11-28 17:17:09 +0530

    largest of three given numbers.

---
+++ coding-assignment/function/function_q05.py
@@ -0,0 +1,7 @@
+# 5. Create a function that returns the maximum of three numbers.
+
+def max_of_three(x, y, z):
+    return max(x, y, z)
+
+# Example usage
+print(max_of_three(10, 20, 15))  # Output: 20
