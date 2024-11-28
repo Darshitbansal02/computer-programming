f05d2c60310d5abb2aeacf9c54e3ff4302c14bd7
Darshitbansal02 <darshitbansal2004@gmail.com>
2024-11-28 19:06:49 +0530

    Count Substrings in a String

--- coding-assignment/string/string_q07.py
+++ coding-assignment/string/string_q07.py
@@ -1,6 +1,6 @@
 # 7. Count Substrings in a String
 # Write a program to count the number of times a substring appears in a string.
-string = "hello world, hello universe"
+input_string = "hello world, hello universe"
 substring = "hello"
-count = string.count(substring)
+count = input_string.count(substring)
 print("Substring count:", count)  # Output: 2
