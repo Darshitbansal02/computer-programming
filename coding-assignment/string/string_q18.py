9f9352b8428cc824d01fee94853bdcce5a1b80f6
Darshitbansal02 <darshitbansal2004@gmail.com>
2024-11-28 19:11:19 +0530

    Frequency of Each Character

--- coding-assignment/string/string_q18.py
+++ coding-assignment/string/string_q18.py
@@ -7,4 +7,4 @@ for char in input_string:
         freq[char] += 1
     else:
         freq[char] = 1
-print(freq)  # Output: {'h':
+print(freq)  # Output: {'h': 1, 'e': 1, 'l': 3, 'o': 2, ' ': 1, 'w': 1, 'r': 1, 'd': 1}
