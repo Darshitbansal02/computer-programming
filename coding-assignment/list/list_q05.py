d31d28c4a2e6769ea0bbfdf974350260031dca37
Darshitbansal02 <darshitbansal2004@gmail.com>
2024-11-28 18:17:12 +0530

    Length of a List Without Using the len() Function

---
+++ coding-assignment/list/list_q05.py
@@ -0,0 +1,6 @@
+# 5. Find the Length of a List Without Using the `len()` Function
+nums = [1, 2, 3, 4, 5]
+length = 0
+for _ in nums:
+    length += 1  # Count the elements
+print(length)  # Output: 5
