df7898e9d97779bbd7cfd11556af89f1a81287c5
Darshitbansal02 <darshitbansal2004@gmail.com>
2024-11-28 19:25:41 +0530

    Tuple into a List

---
+++ coding-assignment/tuple/tuple_q09.py
@@ -0,0 +1,7 @@
+# 9. Convert a Tuple into a List
+# Write a program to convert a tuple into a list.
+input_tuple = (1, 2, 3, 4)
+result_list = []
+for item in input_tuple:
+    result_list.append(item)
+print(result_list)  # Output: [1, 2, 3, 4]
