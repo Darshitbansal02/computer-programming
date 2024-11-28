8c6d89e445b12eabe8dd96f676c66502b09f34d2
Darshitbansal02 <darshitbansal2004@gmail.com>
2024-11-28 17:14:47 +0530

    Function to check even or odd

---
+++ coding-assignment/function/function_q04.py
@@ -0,0 +1,8 @@
+# 4. Write a function to check whether a number is even or odd.
+
+def is_even(num):
+    return num % 2 == 0
+
+# Example usage
+print(is_even(4))  # Output: True
+print(is_even(7))  # Output: False
