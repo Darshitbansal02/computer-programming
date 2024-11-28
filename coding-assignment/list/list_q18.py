75502f69d0a0a66d929277d64dcd8c9e7948af75
Darshitbansal02 <darshitbansal2004@gmail.com>
2024-11-28 18:33:24 +0530

    Flatten a Nested List

---
+++ coding-assignment/list/list_q18.py
@@ -0,0 +1,4 @@
+# 18. Flatten a Nested List
+nested_list = [[1, 2], [3, 4], [5, 6]]
+flattened_list = [item for sublist in nested_list for item in sublist]
+print(flattened_list)  # Output: [1, 2, 3, 4, 5, 6]
