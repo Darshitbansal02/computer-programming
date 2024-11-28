ea651716abb2fe440b5129a2bef7c1df2fd54201
Darshitbansal02 <darshitbansal2004@gmail.com>
2024-11-28 19:08:21 +0530

    Replace Spaces with Underscore

--- coding-assignment/string/string_q12.py
+++ coding-assignment/string/string_q12.py
@@ -1,8 +1,8 @@
 # 12. Replace Spaces with Underscore
 # Write a program to replace all spaces in a string with underscores.
-string = "hello world"
+input_string = "hello world"
 modified_string = ""
-for char in string:
+for char in input_string:
     if char == " ":
         modified_string += "_"
     else:
