b675b066ae359526f7590335cfc84ed851dd0a1b
Darshitbansal02 <darshitbansal2004@gmail.com>
2024-11-28 19:17:23 +0530

    Length of a Tuple

---
+++ coding-assignment/tuple/tuple_q03.py
@@ -0,0 +1,7 @@
+# 3. Find the Length of a Tuple
+# Write a program to find the length of a tuple.
+input_tuple = (10, 20, 30, 40, 50)
+length = 0
+for item in input_tuple:
+    length += 1
+print(length)  # Output: 5
