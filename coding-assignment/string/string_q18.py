ff208c5ed4c86aaebe02c5572a2a8083cb0a5be0
Darshitbansal02 <darshitbansal2004@gmail.com>
2024-11-28 19:10:38 +0530

    Frequency of Each Character

--- coding-assignment/string/string_q18.py
+++ coding-assignment/string/string_q18.py
@@ -1,11 +1,10 @@
 # 18. Find the Frequency of Each Character
 # Write a program to count the frequency of each character in a string.
-string = "hello world"
+input_string = "hello world"
 freq = {}
-for char in string:
+for char in input_string:
     if char in freq:
         freq[char] += 1
     else:
         freq[char] = 1
-print(freq)  # Output: {'h': 1, 'e': 1, 'l': 3, 'o': 2, ' ': 1, 'w': 1, 'r': 1, 'd': 1}
- 
+print(freq)  # Output: {'h':
