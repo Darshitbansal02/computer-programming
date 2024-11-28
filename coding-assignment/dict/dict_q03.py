4a024f5f1704d97e54b735b90302c5032a48f927
Darshitbansal02 <darshitbansal2004@gmail.com>
2024-11-28 16:59:16 +0530

    Merging Multiple Dictionaries Using update()

--- coding-assignment/dict/dict_q03.py
+++ coding-assignment/dict/dict_q03.py
@@ -6,3 +6,4 @@ dict1 = {'name': 'Steve', 'age': 40}
 dict2 = {'age': 45, 'city': 'London'}
 dict1.update(dict2)  # Merges dict2 into dict1
 print(dict1)  # Output: {'name': 'Steve', 'age': 45, 'city': 'London'}
+ 
