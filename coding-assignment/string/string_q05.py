1076c1f81ed251043027b2386b194bbdbab797d0
Darshitbansal02 <darshitbansal2004@gmail.com>
2024-11-28 19:04:35 +0530

    First Non-Repeating Character python Copy code

--- coding-assignment/string/string_q05.py
+++ coding-assignment/string/string_q05.py
@@ -1,8 +1,7 @@
 # 5. Find the First Non-Repeating Character
 # Write a program to find the first non-repeating character in a string.
-string = "swiss"
-for char in string:
-    if string.count(char) == 1:
+input_string = "swiss"
+for char in input_string:
+    if input_string.count(char) == 1:
         print("First non-repeating character:", char)  # Output: "w"
         break
- 
