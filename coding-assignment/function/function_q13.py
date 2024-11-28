3590610279c1a56042fc086b24d4f1b108553f83
Darshitbansal02 <darshitbansal2004@gmail.com>
2024-11-28 17:56:07 +0530

    Function to count how many times a specific character appears

---
+++ coding-assignment/function/function_q13.py
@@ -0,0 +1,7 @@
+# 13. Create a function to count the occurrence of a specific character in a string.
+
+def count_char(s, char):
+    return s.count(char)
+
+# Example usage
+print(count_char("hello", "l"))  # Output: 2
