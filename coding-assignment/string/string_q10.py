74a68d6f9245a672baffe74e28b6d6db603015aa
Darshitbansal02 <darshitbansal2004@gmail.com>
2024-11-28 19:07:32 +0530

    Extract Digits from a String

--- coding-assignment/string/string_q10.py
+++ coding-assignment/string/string_q10.py
@@ -1,8 +1,8 @@
 # 10. Extract Digits from a String
 # Write a program to extract all digits from a string and return them as a string.
-string = "hello 123, how are you 456?"
+input_string = "hello 123, how are you 456?"
 digits = ""
-for char in string:
+for char in input_string:
     if char.isdigit():
         digits += char
 print(digits)  # Output: "123456"
