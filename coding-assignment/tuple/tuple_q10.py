9ebace17618fa053b57bdf01ad94f63c94852dee
Darshitbansal02 <darshitbansal2004@gmail.com>
2024-11-28 19:26:31 +0530

    Repeat Elements of a Tuple

---
+++ coding-assignment/tuple/tuple_q10.py
@@ -0,0 +1,6 @@
+# 10. Repeat Elements of a Tuple
+# Write a program to repeat the elements of a tuple N times.
+input_tuple = (1, 2, 3)
+N = 3
+result_tuple = input_tuple * N
+print(result_tuple)  # Output: (1, 2, 3, 1, 2, 3, 1, 2, 3)
