f59f1432ecd6088a7ed88341064b8784c441baf6
Darshitbansal02 <darshitbansal2004@gmail.com>
2024-11-28 19:13:48 +0530

    Remove Leading Zeros

--- coding-assignment/string/string_q19.py
+++ coding-assignment/string/string_q19.py
@@ -1,8 +1,8 @@
 # 19. Remove Leading Zeros
 # Write a program to remove leading zeros from a string.
-string = "0001234"
+input_string = "0001234"
 result = ""
-for char in string:
+for char in input_string:
     if char != "0" or result:
         result += char
 print(result)  # Output: "1234"
